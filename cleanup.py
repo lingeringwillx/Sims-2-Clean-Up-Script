93dd5861d720ea51057d2e65bdf4924d3265117a
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2023-06-16 21:22:55 +0300

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -22,6 +22,7 @@ def get_total_size():
         pack_path = os.path.join(base_dir, pack.path)
         if os.path.isdir(pack_path):
             total += get_dir_size(pack_path)
+            
     return total / 1024 #GB
     
 #relative path to file excluding pack folder
